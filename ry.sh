47292e4efe0b709576903b03f23ad37214878d88
manzhushaka <18689910753@163.com>
2026-06-27 23:57:55 +0800

    refactor(配置): 将数据源配置迁移为环境变量注入模式
    - 新增 application-dev.yml，替换原有的 application-druid.yml
    - 所有 Druid 数据源参数改为环境变量注入，支持运行时配置
    - application.yml 的 profiles.active 改为
    - ry.bat / ry.sh 启动脚本增加 APP_ENV 环境变量支持
    - 删除硬编码的 application-druid.yml
    - 更新 README.md 为曼珠沙华项目描述

--- ry.sh
+++ ry.sh
@@ -4,6 +4,7 @@ AppName=manzhushaka-ry-admin.jar
 
 # JVM参数
 JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
+APP_ENV=${APP_ENV:-dev}
 APP_HOME=`pwd`
 LOG_PATH=$APP_HOME/logs/$AppName.log
 
@@ -26,7 +27,7 @@ function start()
 	if [ x"$PID" != x"" ]; then
 	    echo "$AppName is running..."
 	else
-		nohup java $JVM_OPTS -jar $AppName > /dev/null 2>&1 &
+		nohup java $JVM_OPTS -Dspring.profiles.active=$APP_ENV -jar $AppName > /dev/null 2>&1 &
 		echo "Start $AppName success..."
 	fi
 }
