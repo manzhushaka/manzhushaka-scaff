827cccc87e5a2bc758aaba16696d1acac5e0be85
manzhushaka <18689910753@163.com>
2026-07-03 10:03:39 +0800

    feat(pii): 创建 manzhushaka-biz-pii 模块骨架

---
+++ manzhushaka-biz-pii/src/test/java/com/manzhushaka/biz/pii/PiiMarkerTest.java
@@ -0,0 +1,13 @@
+package com.manzhushaka.biz.pii;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class PiiMarkerTest {
+
+    @Test
+    void markerClassExists() {
+        assertNotNull(PiiMarker.class);
+    }
+}
